abdd65f43c0c20894d40b1b45d609b3b79f0f39c
M-k-03 <muthukum4r@gmail.com>
2019-05-11 19:51:02 +0530

    Added upgrade Firmware changes

--- src/com/cloakedninjas/cordova/plugins/Country.java
+++ src/com/cloakedninjas/cordova/plugins/Country.java
@@ -5,15 +5,22 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 
 public class Country extends CordovaPlugin {
 
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("get")) {
             String result = Locale.getDefault().getCountry();
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
+            return true;
+        }
+        else if(action.equals("upgradeFirmware")){
+            String deviceId = args.getString(0);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, deviceId));
             return true;
         }
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
